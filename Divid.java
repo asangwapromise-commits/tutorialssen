3ee3db4c9003c1ff137d1ae90953b3b4ff33e33c
Promiz <asangwa.promise@ictuniversity.edu.cm>
2025-12-17 10:17:54 +0000

    Added a file to divid two numbers

---
+++ Divid.java
@@ -0,0 +1,11 @@
+public class Divid {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 2;
+        int result;
+
+        result = (a/b);
+
+        System.out.println("The result is: " + result);
+    }
+}
